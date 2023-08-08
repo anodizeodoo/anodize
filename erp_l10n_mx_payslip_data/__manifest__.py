63044a2721befaf9f9b0a460ad5e7231812bc269
Orlando Palacios Martinez <orlando@soltein.net>
2023-08-07 20:46:48 -0600

    [FIX] module base for rrhh

--- erp_l10n_mx_payslip_data/__manifest__.py
+++ erp_l10n_mx_payslip_data/__manifest__.py
@@ -1,12 +1,11 @@
 # coding: utf-8
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 {
-    "name": "Mexican Payslip Payslip Base data",
-    "version": "14.0.1.0.0",
+    "name": "Mexican Payslip Base data",
+    "version": "15.0.1.0.0",
     "author": "Soltein SA de CV",
     "category": "Localization",
     "website": "https://www.soltein.net",
+    "license": "AGPL-3",
     "depends": [
         "erp_l10n_mx_payslip_cfdi_base",
     ],
