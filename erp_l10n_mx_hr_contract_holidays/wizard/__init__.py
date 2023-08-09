385881047c2b631b9accab267e5ac5ca54cd87a3
Orlando Palacios Martinez <orlando@soltein.net>
2023-08-09 11:50:03 -0600

    [UPG] remove module lineal from multi company repo, add new repo github for rrhh

--- erp_l10n_mx_hr_contract_holidays/wizard/__init__.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-
-from . import generate_holidays
-from . import modify_date_imss
-from . import modify_state_contract
--- erp_l10n_mx_hr_payroll_e8e_vauxo/wizard/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import hr_payroll_payslips_by_employees
-from . import reason_cancelation_sat
-from . import mail_compose_message
-from . import generate_calculation_ptu
-from . import generate_report
