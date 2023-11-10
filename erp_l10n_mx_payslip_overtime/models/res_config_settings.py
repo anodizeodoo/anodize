3162b1e31fc727b4dd9f297b6e1759749aee5f9a
orlandosoltein <89800211+orlandosoltein@users.noreply.github.com>
2023-11-10 12:24:24 -0600

    [UPD] update branch with 16 code for repos rrhh solt

--- erp_l10n_mx_payslip_overtime/models/res_config_settings.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models
-
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    is_extra_hours = fields.Boolean(string='Extra Hours', related='company_id.is_extra_hours', readonly=False, compute_sudo=True)
