856b319da1c1cbd5c4e19960aa16aa6d0673ab01
orlandosoltein <89800211+orlandosoltein@users.noreply.github.com>
2023-11-10 10:38:06 -0600

    [UPD] update branch with 16 code for repos OCA origin

--- erp_l10n_mx_hr_base/models/hr_payroll_structure_type.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models, api
-
-
-class HrPayrollStructureType(models.Model):
-    _inherit = 'hr.payroll.structure.type'
-
-    contract_type_id = fields.Many2one('l10n.mx.payroll.contract.type', string='Contract type', required=1)
-    type_of_benefit_id = fields.Many2one('l10n.mx.type.benefit', string='Type benefit', required=1)
--- erp_l10n_mx_hr_payroll_massive_payment/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-#from . import models
-from . import hr_payslip_massive_payment_view
-from . import hr_payslip_inherit
-from . import res_bank
